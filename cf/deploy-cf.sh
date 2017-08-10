fbc611b52f0b8ed0ad63eed125ee239bb6373ab0
Zakharov Alexey <alexey.zakharov@altoros.com>
2017-08-10 14:04:43 -0700

    Add variables

--- cf/deploy-cf.sh
+++ cf/deploy-cf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 ./cf-concourse/scripts/bosh-connect.sh
 cd cf-deployment
-git checkout v0.4.0
+git checkout ${CF_TAG}
 
 set +e
 vault read -tls-skip-verify -format=yaml secret/cf-deployment >deployment-vars.tmp
@@ -13,7 +13,7 @@ fi
 bosh -n -d cf deploy \
   -o operations/bosh-lite.yml \
   --vars-store=deployment-vars.yml \
-  -v system_domain=${DOMAIN}.nip.io \
+  -v system_domain=${DOMAIN} \
   cf-deployment.yml
 
 cat deployment-vars.yml | ../cf-concourse/scripts/yaml2json.py > deployment-vars.json
