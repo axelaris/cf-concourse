a4c66fe5c1b2fad3616f5b8c7e191bbec81812a4
Zakharov Alexey <alexey.zakharov@altoros.com>
2017-08-10 13:59:49 -0700

    Organize folders

--- cf/deploy-cf.sh
+++ cf/deploy-cf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-./cf-concourse/bosh-connect.sh
+./cf-concourse/scripts/bosh-connect.sh
 cd cf-deployment
 git checkout v0.4.0
 
@@ -8,14 +8,13 @@ vault read -tls-skip-verify -format=yaml secret/cf-deployment >deployment-vars.t
 RES=$?
 set -e
 if [ ${RES} = 0 ]; then
-  cat deployment-vars.tmp | ../cf-concourse/get_data.py >deployment-vars.yml
+  cat deployment-vars.tmp | ../cf-concourse/scripts/get_data.py >deployment-vars.yml
 fi
-rm deployment-vars.tmp
 bosh -n -d cf deploy \
   -o operations/bosh-lite.yml \
   --vars-store=deployment-vars.yml \
   -v system_domain=${DOMAIN}.nip.io \
   cf-deployment.yml
 
-cat deployment-vars.yml | ../cf-concourse/yaml2json.py > deployment-vars.json
+cat deployment-vars.yml | ../cf-concourse/scripts/yaml2json.py > deployment-vars.json
 vault write -tls-skip-verify secret/cf-deployment @deployment-vars.json
