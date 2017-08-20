a1652df0a5b2ec48983d6d87e02f1b7fcba5908c
Zakharov Alexey <alexey.zakharov@altoros.com>
2017-08-20 11:26:03 +0300

    Add vault policy

--- vault/unseal-vault.sh
+++ vault/unseal-vault.sh
@@ -19,3 +19,11 @@ if [ ${SEALED} == true ]; then
   echo --- Authenticating
   cat vault-keys | grep "Token" | sed s/.*Token:\ // | vault auth ${NOTLS} -
 fi
+
+#Integrate concourse
+vault mount ${NOTLS} -path=/concourse -description="Secrets for concourse pipelines" generic
+vault policy-write ${NOTLS} policy-concourse policy.hcl
+vault token-create ${NOTLS} --policy=policy-concourse -period="600h" -format=json >/tmp/token
+TOKEN=`cat /tmp/token | jq -r ".auth.client_token"`
+#vault write ${NOTLS} concourse/main/vault-root-token value=
+#vault write ${NOTLS} concourse/main/vault-address value=
