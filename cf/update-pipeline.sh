7a4987685e9bac61649a184b96e69125d50a4c50
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-08-30 16:50:39 +0300

    Update pipelines non-interactively

--- cf/update-pipeline.sh
+++ cf/update-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fly -t $1 set-pipeline -p cf -c pipeline.yml -l pipeline-vars.yml -l ../bosh-vars.yml
+fly -t $1 set-pipeline -n -p cf -c pipeline.yml -l pipeline-vars.yml -l ../bosh-vars.yml
