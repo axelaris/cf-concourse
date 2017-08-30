a689d4e371897cb1b2a31a8c1995abe662fea3eb
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-08-30 18:37:32 +0300

    Merge pipelines

--- concourse/update-pipeline.sh
+++ concourse/update-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fly -t $1 set-pipeline -n -p concourse -c pipeline.yml -l ../bosh-vars.yml
+fly -t $1 set-pipeline -n -p main -c pipeline.yml -l ../bosh-vars.yml -l ../cf/pipeline-vars.yml
