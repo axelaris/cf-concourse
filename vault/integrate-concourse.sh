1260fe3fdbb6c4962f214a576fa31ef64533c9c4
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-01 11:38:47 +0300

    Fix typo

--- vault/integrate-concourse.sh
+++ vault/integrate-concourse.sh
@@ -2,5 +2,5 @@
 vault mount ${NOTLS} -path=/concourse -description="Secrets for concourse pipelines" generic
 vault policy-write ${NOTLS} policy-concourse cf-concourse/vault/policy.hcl
 vault token-create ${NOTLS} --policy=policy-concourse -period="600h" -format=json | jq -r ".auth.client_token" >keys/concourse_token
-vault write ${NOTLS} concourse/main/vault-root-token value=`cat keys/vault-keys | grep "Token" | sed s/.*Token:\ //`
+vault write ${NOTLS} concourse/main/vault-root-token value=`cat ${KEYS} | grep "Token" | sed s/.*Token:\ //`
 vault write ${NOTLS} concourse/main/vault-address value=${VAULT_ADDR}
