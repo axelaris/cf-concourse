f6c718ee4345187e634627872a675ecc8fc0a5c5
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-02 00:07:21 +0300

    Swap --var-file to --var due to processing bug

--- concourse/deploy-concourse.sh
+++ concourse/deploy-concourse.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 ./cf-concourse/scripts/bosh-connect.sh
+ADDR=`cat keys/vault_addr`
+TOKEN=`cat keys/concourse_token`
 cd cf-concourse/concourse
 
-bosh -n -d concourse deploy -o ops-vault.yml --var-file=VAULT_ADDR=../../keys/vault_addr --var-file=TOKEN=../../keys/concourse_token concourse.yml
+bosh -n -d concourse deploy -o ops-vault.yml --var=VAULT_ADDR=${ADDR} --var=TOKEN=${TOKEN} concourse.yml
 
