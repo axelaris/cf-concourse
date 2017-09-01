b60b3c96ec8f966a412d9fdb30e104265f4e8eab
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-02 00:39:19 +0300

    More debug messages

--- concourse/deploy-concourse.sh
+++ concourse/deploy-concourse.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 ./cf-concourse/scripts/bosh-connect.sh
-ADDR=`cat keys/vault_addr`
-TOKEN=`cat keys/concourse_token`
-cd cf-concourse/concourse
-
-bosh -n -d concourse deploy -o ops-vault.yml --var=VAULT_ADDR=${ADDR} --var=TOKEN=${TOKEN} concourse.yml
-
+if [ -d keys ]; then
+  echo "=== Updating Concourse"
+  ADDR=`cat keys/vault_addr`
+  TOKEN=`cat keys/concourse_token`
+  cd cf-concourse/concourse
+  bosh -n -d concourse deploy -o ops-vault.yml --var=VAULT_ADDR=${ADDR} --var=TOKEN=${TOKEN} concourse.yml
+else 
+  echo "=== No keys found. Skipping update"
+fi
