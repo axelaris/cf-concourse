2f44245e81173a273a6439cc44de215873d70042
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-02 00:23:34 +0300

    More debug for CF deployment

--- cf/deploy-cf.sh
+++ cf/deploy-cf.sh
@@ -8,7 +8,10 @@ vault read -tls-skip-verify -format=yaml secret/cf-deployment >deployment-vars.t
 RES=$?
 set -e
 if [ ${RES} = 0 ]; then
+  echo "=== Found CF deployment. Retreiving variables"
   cat deployment-vars.tmp | ../cf-concourse/scripts/get_data.py >deployment-vars.yml
+else
+  echo "=== No deployment vars found. Deploying fresh CF"
 fi
 bosh -n -d cf deploy \
   -o operations/bosh-lite.yml \
@@ -17,4 +20,5 @@ bosh -n -d cf deploy \
   cf-deployment.yml
 
 cat deployment-vars.yml | ../cf-concourse/scripts/yaml2json.py > deployment-vars.json
+echo "=== Saving deployment vars into Vault"
 vault write -tls-skip-verify secret/cf-deployment @deployment-vars.json
