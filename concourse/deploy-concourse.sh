a48339c121119f816921a90c92594c52c7daaca6
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-01 23:07:26 +0300

    Fix path

--- concourse/deploy-concourse.sh
+++ concourse/deploy-concourse.sh
@@ -2,5 +2,5 @@
 ./cf-concourse/scripts/bosh-connect.sh
 cd cf-concourse/concourse
 
-bosh -n -d concourse deploy -o ops-vault.yml --var-file=VAULT_ADDR=keys/vault_addr --var-file=TOKEN=keys/concourse_token concourse.yml
+bosh -n -d concourse deploy -o ops-vault.yml --var-file=VAULT_ADDR=../../keys/vault_addr --var-file=TOKEN=../../keys/concourse_token concourse.yml
 
