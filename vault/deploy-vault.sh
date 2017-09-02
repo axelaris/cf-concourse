a2eaacdedf074d537239d7f242990805e6c7be98
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-02 19:04:24 +0300

    Fix path

--- vault/deploy-vault.sh
+++ vault/deploy-vault.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./cf-concourse/scripts/bosh-connect.sh
-cd vault-boshrelease/manifests
-bosh -n -d vault deploy --vars-store=vault-vars.yml -o ops-bosh.yml vault.yml
+cd cf-concourse/vault
+bosh -n -d vault deploy --vars-store=vault-vars.yml -o ops-bosh.yml ../../vault-boshrelease/manifests/vault.yml
 IP=`bosh -d vault --column=IPs --json vms | jq -r ".Tables[0].Rows[0].ips"`
 echo "=== Vault server ${IP} will be used as main"
 echo "https://${IP}:8200" > ../../vault/addr
