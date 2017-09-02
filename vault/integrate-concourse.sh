f82d71e5b97a3fdc7b7652f115791dde563fd4f2
Alexey Zakharov <alexey.zakharov@altoros.com>
2017-09-02 15:49:36 +0300

    Replace Vault root token for CF with specific one

--- vault/integrate-concourse.sh
+++ vault/integrate-concourse.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 vault mount ${NOTLS} -path=/concourse -description="Secrets for concourse pipelines" generic
-vault policy-write ${NOTLS} policy-concourse cf-concourse/vault/policy.hcl
+vault policy-write ${NOTLS} policy-concourse cf-concourse/vault/policy-concourse.hcl
 vault token-create ${NOTLS} --policy=policy-concourse -period="600h" -format=json | jq -r ".auth.client_token" >keys/concourse_token
-vault write ${NOTLS} concourse/main/vault-root-token value=`cat ${KEYS} | grep "Token" | sed s/.*Token:\ //`
+vault policy-write ${NOTLS} policy-secret cf-concourse/vault/policy-secret.hcl
+vault token-create ${NOTLS} --policy=policy-secret -period="600h" -format=json | jq -r ".auth.client_token" >keys/secret_token
+vault write ${NOTLS} concourse/main/secret-token value=`cat keys/secret_token`
 vault write ${NOTLS} concourse/main/vault-address value=${VAULT_ADDR}
 vault write ${NOTLS} concourse/main/cf-tag value=v0.4.0
 # This is for BOSH Lite only
